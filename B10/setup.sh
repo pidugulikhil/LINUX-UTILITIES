d6ab6aca172db9abee3f9978e8d97c12ef05411d
likhil-pidugu <pidugulikhil@gmail.com>
2025-06-15 20:11:14 +0530

    Rename folder B7 to B10, remove legacy scripts (b7.sh, b8.sh, b9.sh), and add new setup.sh

---
+++ B10/setup.sh
@@ -0,0 +1,10 @@
+# Installing Required tools :
+apt install x11-xserver-utils
+sudo apt install yad
+sudo apt coreutils
+
+# Providing permissions to Script :
+chmod +x b10.sh
+
+# Running the Script :
+bash b10.sh &
